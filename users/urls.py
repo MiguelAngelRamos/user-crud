70b33b6558df819c57cd35a92d53553537bb9052
Miguel Angel Ramos <return.infox@gmail.com>
2024-06-03 20:54:21 -0400

    todo usuarios2

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.user_list, name='user_list'),
     path('add/', views.add_user, name='add_user' ),
+    path('export/', views.export_user_to_excel, name='export_users_to_excel'),
+    path('delete/<int:user_id>', views.delete_user, name='delete_user')
 ]
+
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 # Lo necesario para "auth"
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -48,5 +48,43 @@ def add_user(request):
             
 # export_users_to_excel require este decorador @admin_required(login_url='/login/')
 
+def export_user_to_excel(request):
+    # Obtiene todos los usuarios de la base de datos
+    users = User.objects.all()
 
+    # Establece la cabecera para indicar que la respuesta debe ser descargada como un archivo adjunto  users.xlsx
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        headers = {'Content-Disposition':'attachment; filename=users.xlsx'}
+    )
+    
+    # Crea un nuevo libre de trabajo excel
+    workbook = openpyxl.Workbook()
+    
+    # Obtenie la hoja de trabajo activa y le pone un titulo 'users'
+    worksheet = workbook.active;
+    worksheet.title = 'Users'
+    
+    columns = ['Username', 'Email', 'First Name', 'Last Name']
+    row_num = 1
+    
+    for col_num, column_title in enumerate(columns, 1):
+        worksheet.cell(row=row_num, column=col_num, value=column_title)
+        
+    for user in users:
+        row_num +=1;
+        row = [user.username, user.email, user.first_name, user.last_name]
+        for col_num, column_value in enumerate(row, 1):
+            worksheet.cell(row=row_num, column=col_num, value=column_value)
+    
+    
+    workbook.save(response)
+    return response;
+        
 
+## Eliminar un usuario
+
+def delete_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.delete()
+    return redirect('user_list')
